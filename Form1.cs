a8a056b8c8a7ea40b599f4dd77d2b9e8dc6a3d18
ira23 <48791398+ira23@users.noreply.github.com>
2019-06-01 12:23:46 +0900

    15zad

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15zad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Complate!");
+            decimal umn = Convert.ToDecimal(textBox1.Text) * Convert.ToDecimal(textBox1.Text);
+            textBox2.Text = umn.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int number;
+            number = Int32.Parse(textBox1.Text);
+            System.Convert.ToString(number);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int number;
+            number = Int32.Parse(textBox1.Text);
+            System.Convert.ToString(number);
+        }
+    }
+}
